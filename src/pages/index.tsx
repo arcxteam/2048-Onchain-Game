85817fb49a0805ed402144064372ca426c89e3b6
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-08 22:47:26 +0700

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,19 @@
+import Board from '@/components/Board';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Head from 'next/head';
 
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Play 2048!</title>
+      </Head>
+      <main className="mx-auto grid h-screen max-w-lg items-center p-4 py-8">
+        <Header />
+        <Board />
+        <Footer />
+      </main>
+    </>
+  );
+}
