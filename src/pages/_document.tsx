0acf8dad51867e0a9879c72f729fe81449f0108c
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-11 13:37:06 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-180x180.png"
         />
         <link
           rel="icon"
