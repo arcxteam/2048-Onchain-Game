76270d3777bae8305dbf50c497f44c8efebbafb2
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-12 17:41:46 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
-import { BlockchainProvider } from '@/src/minikitprovider';
+import { BlockchainProvider } from '@/minikitprovider'; // Perbaiki dari '@/src/minikitprovider'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [flag, setFlag] = useState(false);
