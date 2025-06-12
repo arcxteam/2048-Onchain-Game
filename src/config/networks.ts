1a4c4a90a5692025842f447d4094e5ef0f0be3a9
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-12 14:03:54 +0700

    Update networks.ts

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -1,7 +1,10 @@
 export const networkConfigs = {
-  sepolia: { chainId: 11155111, rpcUrl: 'https://eth-sepolia.public.blastapi.io' },
-  holesky: { chainId: 17000, rpcUrl: 'https://ethereum-holesky-rpc.publicnode.com' },
-  '0g-testnet': { chainId: 16601, rpcUrl: 'https://evmrpc-testnet.0g.ai' },
+  '0g-testnet': {
+    chainId: 16601,
+    rpcUrl: 'https://evmrpc-testnet.0g.ai',
+  },
+  // add other network (sepolia, holesky)
 };
 
 export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '0xdF0d5abC614EF45C4bCEA121624644523BAc80b7';
+export const abi = []; // Placeholder, src/pages/api/ABI.json
