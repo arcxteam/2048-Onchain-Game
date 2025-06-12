e96bcbeefe3ee7352564acf4d8ca0a5944fffcc2
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-12 16:12:51 +0700

    Update action.ts

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,7 +1,7 @@
 import { type Direction } from '@/types/Direction';
-
 import { ActionType } from '@/types/ActionType';
 import { type ActionModel } from '@/types/Models';
+import { slideBoard } from '@/utils/board';
 
 function resetAction(size: number): ActionModel {
   return {
@@ -10,23 +10,46 @@ function resetAction(size: number): ActionModel {
   };
 }
 
-function undoAction(): ActionModel {
-  return {
-    type: ActionType.UNDO,
-  };
-}
-
 function moveAction(direction: Direction): ActionModel {
   return {
     type: ActionType.MOVE,
     value: direction,
+    async execute(contract: ethers.Contract, gameId: string, board: number[]) {
+      if (contract && gameId) {
+        const newBoard = slideBoard(board, direction);
+        if (newBoard) {
+          await contract.play(gameId, direction, newBoard[0]); // Send the first element as uint128
+          return newBoard;
+        }
+      }
+    },
   };
 }
 
-function dismissAction(): ActionModel {
+function endGameAction(gameId: string): ActionModel {
   return {
-    type: ActionType.DISMISS,
+    type: 'END_GAME',
+    value: gameId,
+    async execute(contract: ethers.Contract) {
+      if (contract && gameId) {
+        const tx = await contract.endGame(gameId);
+        await tx.wait();
+      }
+    },
   };
 }
 
-export { resetAction, undoAction, moveAction, dismissAction };
+function claimNFTAction(gameId: string): ActionModel {
+  return {
+    type: 'CLAIM_NFT',
+    value: gameId,
+    async execute(contract: ethers.Contract) {
+      if (contract && gameId) {
+        const tx = await contract.claimNFT(gameId);
+        await tx.wait();
+      }
+    },
+  };
+}
+
+export { resetAction, moveAction, endGameAction, claimNFTAction };
