ea2beb08becabf26060677f613d48ef169c309d4
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-13 12:03:23 +0700

    Update web3auth.tsx

--- src/providers/web3auth.tsx
+++ src/providers/web3auth.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { Web3AuthProvider } from "@web3auth/modal/react";
 import { WagmiProvider } from "wagmi";
-import { configureChains, createConfig, mainnet, sepolia, holesky} from "wagmi";
+import { configureChains, createConfig, mainnet, sepolia, holesky } from "wagmi";
 import { publicProvider } from "wagmi";
 
 const { chains, publicClient } = configureChains(
