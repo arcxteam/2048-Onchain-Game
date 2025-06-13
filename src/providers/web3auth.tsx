d3219fa5f72f69e6f82cb2184383d75289209857
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-13 11:16:24 +0700

    Update web3auth.tsx

--- src/providers/web3auth.tsx
+++ src/providers/web3auth.tsx
@@ -4,18 +4,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { Web3AuthProvider } from "@web3auth/modal/react";
 import { WagmiProvider } from "wagmi";
-import { configureChains, chain } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
+import { configureChains, createConfig, mainnet, sepolia } from "wagmi"; // Perbaiki impor
+import { publicProvider } from "wagmi"; // Impor langsung dari wagmi
 
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.sepolia, { ...chain.custom, id: 16601, name: '0g Testnet', rpcUrls: { default: { http: ['https://evmrpc-testnet.0g.ai'] } } }],
+const { chains, publicClient } = configureChains(
+  [mainnet, sepolia, { id: 16601, name: '0g Testnet', rpcUrls: { default: { http: ['https://evmrpc-testnet.0g.ai'] } } }],
   [publicProvider()]
 );
 
-const wagmiConfig = {
-  autoConnect: false, // nonactive connection auto
-  provider,
-};
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  publicClient,
+});
 
 const web3AuthOptions = {
   clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID!,
