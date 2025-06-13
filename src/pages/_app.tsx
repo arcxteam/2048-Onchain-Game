03129d83567f5f64696a21b9bf7349753e480019
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-13 07:50:43 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,15 @@
 import { store } from '@/store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
-import { BlockchainProvider } from '@/providers/minikitprovider';
+import { Web3AuthProviders } from '@/providers/web3auth';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [flag, setFlag] = useState(false);
-  useEffect(() => {
-    setFlag(true);
-  }, []);
   return (
-    flag && (
-      <Provider store={store}>
-        <BlockchainProvider>
-          <Component {...pageProps} />
-        </BlockchainProvider>
-      </Provider>
-    )
+    <Provider store={store}>
+      <Web3AuthProviders>
+        <Component {...pageProps} />
+      </Web3AuthProviders>
+    </Provider>
   );
 }
