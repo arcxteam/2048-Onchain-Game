4578a855e30e6ddb67545e9ed6acb473ec114225
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-14 11:51:27 +0700

    Update web3auth.tsx

--- src/providers/web3auth.tsx
+++ src/providers/web3auth.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { Web3AuthProvider } from "@web3auth/modal/react";
 import { WagmiProvider } from "@web3auth/modal/react/wagmi";
+import { networkConfigs } from '@/config/networks';
 
 type Props = {
   children: ReactNode;
@@ -17,7 +18,12 @@ export function Web3AuthProviders({ children }: Props) {
       config={{
         web3AuthOptions: {
           clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID!,
-          web3AuthNetwork: "sapphire_devnet",
+          web3AuthNetwork: "testnet", // Gunakan testnet generik untuk 0g-testnet
+          chainConfig: {
+            chainNamespace: "eip155",
+            chainId: `0x${networkConfigs['0g-testnet'].chainId.toString(16)}`,
+            rpcTarget: networkConfigs['0g-testnet'].rpcUrl,
+          },
         },
       }}
     >
