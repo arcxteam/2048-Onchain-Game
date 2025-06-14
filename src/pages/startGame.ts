319ca1195a3fdc1ea865be9f6b6d3c667056f388
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-14 11:41:20 +0700

    Update and rename startpayment.ts to startGame.ts

--- src/pages/startGame.ts
+++ src/pages/startGame.ts
@@ -4,6 +4,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const gameId = `game-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+  const gameId = ethers.utils.formatBytes32String(`game-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`);
   res.status(200).json({ gameId });
 }
