ea0177aba42530f1f431776cfdde9c5e0c4702a9
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-15 21:02:22 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
-// @ts-check
-
-
-/** @type {import('next').NextConfig} */
+import { defineNextConfig } from 'next';
 
 const nextConfig = {
-  reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://10.0.10.247:3000", // Sesuaikan dengan alamat Codespace
+  ],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "react-native": false,
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@react-native-async-storage/async-storage": "@react-native-async-storage/async-storage",
+    };
+    return config;
+  },
 };
 
-
-export default nextConfig
+export default defineNextConfig(nextConfig);
