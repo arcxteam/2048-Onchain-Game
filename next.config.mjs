abb74f55fa83e808a59e38d64eaf26cd51e89927
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-06-20 14:32:46 +0700

    Update next.config.mjs FIX 0.2.0

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://10.0.11.161:3000",
+  ],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       "react-native": false,
       "crypto": "crypto-browserify",
       "stream": "stream-browserify",
-      "buffer": "buffer/"
+      "buffer": "buffer/",
     };
 
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@react-native-async-storage/async-storage": "@react-native-async-storage/async-storage"
+      "@react-native-async-storage/async-storage": "@react-native-async-storage/async-storage",
     };
 
     return config;
   },
-  swcMinify: true,
+  // swcMinify: true,
   transpilePackages: [
     '@web3auth/modal',
     '@web3auth/base',
